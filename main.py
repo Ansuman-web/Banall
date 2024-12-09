fd93045bf7960a5c691e907e589694975505e07c
ZEUS-GIT <ansumanmohanty2006@gmail.com>
2024-12-09 19:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ async def leave(z):
 
 @sree.on(events.NewMessage)
 async def ver(events):
-    events = NewMessage
+    events = "https://t.me/Rosie_clone_roobot"
     await events.main(str(e))
 
 
